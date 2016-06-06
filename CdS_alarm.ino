750bc7f2dbf5ee9b08875c62a2c52443ff318da1
Juhyun Kim <mtinet@hanmail.net>
2016-06-07 08:37:21 +0900

    Add files via upload

---
+++ CdS_alarm.ino
@@ -0,0 +1,37 @@
+const int buzzerpin=8;
+const int ledpin=6;
+const int sensorpin=A5;
+int val =0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(buzzerpin, OUTPUT);
+  pinMode(ledpin, OUTPUT);
+  val = analogRead(sensorpin)+200;
+  Serial.print("Initial Value:");
+  Serial.println(val);
+}
+
+void loop()
+{
+  int rate = analogRead(sensorpin);
+  Serial.println(rate);
+  
+  if(rate>val)
+  {
+    digitalWrite(buzzerpin, HIGH);
+    digitalWrite(ledpin, HIGH);
+    delay(50);               
+    digitalWrite(buzzerpin, LOW); 
+    digitalWrite(ledpin, LOW);    
+    delay(50);
+  }
+  else
+  {
+    digitalWrite(ledpin, LOW);
+    delay(200);
+    digitalWrite(ledpin,HIGH);
+    delay(50);
+  }
+}
